bd8317d2a7011bbf578fffb37b2a288e80b1747a
UserUnavailable <klebersilva2025@gmail.com>
2019-08-04 11:49:13 -0300

    added /emotes, /base64 commands and some code improvement

--- api/bot/index.sh
+++ api/bot/index.sh
@@ -1,17 +1,37 @@
 declare -A twitch=(
-[KappaPride]="CAADAQADJQcAAsTJswNX-kZLiMEjjRYE"
-[PogChamp]="CAADAQADMwcAAsTJswOVakNX-eTErBYE"
-[Kappa]="CAADAQADEQcAAsTJswOwrIcn0_a7ixYE"
-[4Head]="CAADAQADEwcAAsTJswP8QrR3rJSgRBYE"
-[LUL]="CAADAQADKQcAAsTJswMfEVJbyr4KCRYE"
+	[KappaPride]="CAADAQADJQcAAsTJswNX-kZLiMEjjRYE"
+	[PogChamp]="CAADAQADMwcAAsTJswOVakNX-eTErBYE"
+	[Kappa]="CAADAQADEQcAAsTJswOwrIcn0_a7ixYE"
+	[4Head]="CAADAQADEwcAAsTJswP8QrR3rJSgRBYE"
+	[LUL]="CAADAQADKQcAAsTJswMfEVJbyr4KCRYE"
 )
 
-function send_sticker() {
-	curl -s -d "chat_id=${1}&sticker=${2}" -X POST ${BOT}/sendSticker
-}
+## Chat message about availables Emotes
+emotes_list="*A list of availables Emotes:*"$'\n'
+for emote in ${!twitch[@]}; do
+	emotes_list+="- ${emote}"$'\n'
+done
 
 function send_msg() {
-	curl -s -d "chat_id=${1}&text=${2}" -X POST ${BOT}/sendMessage
+	case ${1} in
+		--reply)   curl -s -F "chat_id=${2}"             \
+			           -F "text=${3}"                \
+				   -F "parse_mode=Markdown"      \
+				   -F "reply_to_message_id=${4}" \
+		          	   -X POST ${BOT}/sendMessage
+			 	   ;;
+		
+		--sticker) curl -s -F "chat_id=${2}"             \
+			           -F "sticker=${3}"             \
+			   	   -X POST ${BOT}/sendSticker
+			   	   ;;
+		
+		*)         curl -s -F "chat_id=${1}"             \
+			           -F "text=${2}"                \
+			           -F "parse_mode=Markdown"      \
+		   	           -X POST ${BOT}/sendMessage
+			           ;;
+   	esac
 }
 
 handler() {
@@ -21,13 +41,34 @@ handler() {
 	DATA=$(jq -r '.body' < $1)
 
 	MSG=$(jq -r '.message.text' <<< ${DATA})
-        CHAT=$(jq -r '.message.chat.id' <<< ${DATA})
+	CHAT=$(jq -r '.message.chat.id' <<< ${DATA})
+
+	if [[ ${MSG} =~ ^/ ]];then
+		case $(awk '{print substr($1,2)}' <<< ${MSG}) in
+			## /emotes - list of emotes
+			emotes) send_msg ${CHAT} "${emotes_list}"
+				;;
+			
+			## /base64 - return a base64 string
+			base64) if REPLY=$(jq -re '.message.reply_to_message' <<< ${DATA});then	
+					REPLY_MSG=$(jq -r '.text' <<< ${REPLY})
+					REPLY_ID=$(jq -r '.message_id' <<< ${REPLY})
+
+					send_msg --reply ${CHAT} "$(base64 <<< ${REPLY_MSG})" ${REPLY_ID}
+				else
+					send_msg ${CHAT} "*Error*: please reply to a message."
+				fi
+				;;
+		esac
+	fi
 
 	## Twitch Emojis
-	for iterator in ${!twitch[@]}; do
-		if [[ ${MSG} =~ ${iterator} ]]; then
-			send_sticker ${CHAT} ${twitch[${iterator}]}
+	for emote in ${!twitch[@]}; do
+		if [[ ${MSG} =~ ${emote} ]]; then
+			send_msg --sticker ${CHAT} ${twitch[${emote}]}
 			break
 		fi
 	done
+
+	http_response_code 200
 }
